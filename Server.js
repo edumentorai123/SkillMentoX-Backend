10769cd9a2627528aaed437f50d0ff59fdcd0e49
muhammed dilshad tk <dilshad@DILSHADs-MacBook-Air.local>
2025-08-30 16:21:40 +0530

    set admin

--- Server.js
+++ Server.js
@@ -22,7 +22,6 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true })); 
 
-app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/mentor", mentorRoutes);
