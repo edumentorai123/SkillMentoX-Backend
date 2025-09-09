ca2a32caeafeb02890a41631fd6011f5d7a82a06
muhammed dilshad tk <dilshad@DILSHADs-MacBook-Air.local>
2025-09-09 17:43:07 +0530

    user and mentor

--- routes/mentorRoutes.js
+++ routes/mentorRoutes.js
@@ -3,43 +3,53 @@ import {
   createMentorRequest,
   createOrUpdateMentorProfile,
   deleteMentorDocument,
-  getMentorProfile,
+
   getMentorRequests,
   approveMentorRequest,
   rejectMentorRequest,
+  getMentorProfiles,
+
 } from "../controllers/mentorController.js";
 import { protect, authorize } from "../middleware/authMiddleware.js";
 import upload from "../middleware/upload.js";
 import { courseCategories } from "../data/courseCategories.js";
+import Mentor from "../models/mentor.js";
 
 const router = express.Router();
 
-// Course categories
 router.get("/courseCategories", (req, res) => {
   res.json({ success: true, data: courseCategories });
 });
 
-
-// Mentor profile routes
 router.post(
   "/updateProfile",
   protect,
   authorize("mentor"),
   upload.fields([
-    { name: "profilePicture", maxCount: 1 }, // single
-    { name: "idProof", maxCount: 10 }, // multiple
-    { name: "qualificationProof", maxCount: 10 }, // multiple
-    { name: "cv", maxCount: 10 }, // multiple
+    { name: "profilePicture", maxCount: 1 },
+    { name: "idProof", maxCount: 10 },
+    { name: "qualificationProof", maxCount: 10 },
+    { name: "cv", maxCount: 10 },
   ]),
   createOrUpdateMentorProfile
 );
-router.get("/profile", protect, getMentorProfile);
+
+
+
 router.delete("/document", protect, authorize("mentor"), deleteMentorDocument);
-
-
 router.post("/mentor-request", protect, createMentorRequest);
 router.get("/mentor-requests", protect, getMentorRequests);
-router.patch("/mentor-requests/:id/approve", protect, authorize("admin"), approveMentorRequest);
-router.patch("/mentor-requests/:id/reject", protect, authorize("admin"), rejectMentorRequest);
+router.patch(
+  "/mentor-requests/:id/approve",
+  protect,
+  authorize("admin"),
+  approveMentorRequest
+);
+router.patch(
+  "/mentor-requests/:id/reject",
+  protect,
+  authorize("admin"),
+  rejectMentorRequest
+);
 
 export default router;
