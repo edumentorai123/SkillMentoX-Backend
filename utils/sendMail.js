ea8406c293551425594312a9abdf93e15c1e54f5
iamfaisalpk <iamfaizy123@gmail.com>
2026-03-19 12:00:39 +0530

    fixing some isue

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -3,19 +3,14 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT || 587),
-  secure: false, // true for 465, false for other ports
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000, // 10 seconds timeout
+  connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 15000,
-  tls: {
-    rejectUnauthorized: false, // Helps with some network environments
-  },
 });
 
 const createEmailTemplate = (content, title = "SkillMentorX") => {
